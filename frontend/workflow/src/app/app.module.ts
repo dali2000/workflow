bc4da2d4dd583cb23cba0effbb4b37b982f66fe2
dali2000 <medalikebair@gmail.com>
2022-03-28 14:09:21 +0100

    add routes

--- frontend/workflow/src/app/app.module.ts
+++ frontend/workflow/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  { path: 'SignUp', component: SignUpComponent }
+];  // <-- import RouterModule and define routes
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpClientModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
